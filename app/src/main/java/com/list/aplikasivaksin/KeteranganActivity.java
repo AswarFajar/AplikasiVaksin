d7253b72b12aa05568d3426e6b2eced3b55a0756
AswarFajar <60900120023@uin-alauddin.ac.id>
2022-12-09 21:19:06 +0800

    KELOMPOK 7 MOBILE

---
+++ app/src/main/java/com/list/aplikasivaksin/KeteranganActivity.java
@@ -0,0 +1,14 @@
+package com.list.aplikasivaksin;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class KeteranganActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_keterangan);
+    }
+}
